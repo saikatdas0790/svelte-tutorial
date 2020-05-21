c9cbcdffb6292073ad27739c631f00367a0569d4
Saikat Das <saikatdas0790@gmail.com>
2020-05-21 17:02:44 +0530

    introduction - styling

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,10 @@
   import IntroductionBasics from "./1-introduction/a-basics.svelte";
   import IntroductionAddingData from "./1-introduction/b-adding-data.svelte";
   import IntroductionDynamicAttributes from "./1-introduction/c-dynamic-attributes.svelte";
+  import IntroductionStyling from "./1-introduction/d-styling.svelte";
 </script>
 
 <!-- <IntroductionBasics></IntroductionBasics> -->
 <!-- <IntroductionAddingData></IntroductionAddingData> -->
-<IntroductionDynamicAttributes></IntroductionDynamicAttributes>
+<!-- <IntroductionDynamicAttributes></IntroductionDynamicAttributes> -->
+<IntroductionStyling></IntroductionStyling>
