d674faa8aa7c3fcf4fa6d2582b7d64e4ad86a906
Saikat Das <saikatdas0790@gmail.com>
2020-05-21 16:59:39 +0530

    introduction - dynamic attributes

---
+++ src/1-introduction/c-dynamic-attributes.svelte
@@ -0,0 +1,6 @@
+<script>
+  const src = "/images/1-introduction/image.gif";
+  const name = "Saikat";
+</script>
+
+<img {src} alt="{name} dances" />
--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import IntroductionBasics from "./1-introduction/a-basics.svelte";
   import IntroductionAddingData from "./1-introduction/b-adding-data.svelte";
+  import IntroductionDynamicAttributes from "./1-introduction/c-dynamic-attributes.svelte";
 </script>
 
 <!-- <IntroductionBasics></IntroductionBasics> -->
-<IntroductionAddingData></IntroductionAddingData>
+<!-- <IntroductionAddingData></IntroductionAddingData> -->
+<IntroductionDynamicAttributes></IntroductionDynamicAttributes>
