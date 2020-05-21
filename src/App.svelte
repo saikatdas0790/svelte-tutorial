fe0cd0db9f22d2c388ef46a895a96cfb91549d84
Saikat Das <saikatdas0790@gmail.com>
2020-05-21 17:16:45 +0530

    introduction - nested components

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
-  import IntroductionBasics from "./1-introduction/a-basics.svelte";
-  import IntroductionAddingData from "./1-introduction/b-adding-data.svelte";
-  import IntroductionDynamicAttributes from "./1-introduction/c-dynamic-attributes.svelte";
-  import IntroductionStyling from "./1-introduction/d-styling.svelte";
+  // import IntroductionBasics from "./1-introduction/a-basics.svelte";
+  // import IntroductionAddingData from "./1-introduction/b-adding-data.svelte";
+  // import IntroductionDynamicAttributes from "./1-introduction/c-dynamic-attributes.svelte";
+  // import IntroductionStyling from "./1-introduction/d-styling.svelte";
+  import IntroductionNestedComponents from "./1-introduction/e-nested-components/index.svelte";
 </script>
 
 <!-- <IntroductionBasics></IntroductionBasics> -->
 <!-- <IntroductionAddingData></IntroductionAddingData> -->
 <!-- <IntroductionDynamicAttributes></IntroductionDynamicAttributes> -->
-<IntroductionStyling></IntroductionStyling>
+<!-- <IntroductionStyling></IntroductionStyling> -->
+<IntroductionNestedComponents></IntroductionNestedComponents>
