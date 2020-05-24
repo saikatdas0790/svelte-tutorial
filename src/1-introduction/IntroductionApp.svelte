0fee53ef45573a540c8e1bdb05a37d6e7b1b6b3e
Saikat Das <saikatdas0790@gmail.com>
2020-05-24 09:07:15 +0530

    introduction - styling

--- src/1-introduction/IntroductionApp.svelte
+++ src/1-introduction/IntroductionApp.svelte
@@ -2,12 +2,12 @@
   import IntroductionBasics from "./a-basics/index.svelte";
   import IntroductionAddingData from "./b-adding-data/index.svelte";
   import IntroductionDynamicAttributes from "./c-dynamic-attributes/index.svelte";
-  // import IntroductionStyling from "./1-introduction/d-styling.svelte";
+  import IntroductionStyling from "./d-styling/index.svelte";
   // import IntroductionNestedComponents from "./1-introduction/e-nested-components/index.svelte";
 </script>
 
 <!-- <IntroductionBasics></IntroductionBasics> -->
 <!-- <IntroductionAddingData></IntroductionAddingData> -->
-<IntroductionDynamicAttributes></IntroductionDynamicAttributes>
-<!-- <IntroductionStyling></IntroductionStyling> -->
+<!-- <IntroductionDynamicAttributes></IntroductionDynamicAttributes> -->
+<IntroductionStyling></IntroductionStyling>
 <!-- <IntroductionNestedComponents></IntroductionNestedComponents> -->
