4519e9323780e76a556aa6a2cc2f8f7e6ab7cdfe
Saikat Das <saikatdas0790@gmail.com>
2020-06-11 00:04:26 +0530

    reactivity - updating arrays and objects

--- src/2-reactivity/ReactivityApp.svelte
+++ src/2-reactivity/ReactivityApp.svelte
@@ -2,8 +2,10 @@
   import ReactivityAssignments from "./a-assignments/index.svelte";
   import ReactivityDeclarations from "./b-declarations/index.svelte";
   import ReactivityStatements from "./c-statements/index.svelte";
+  import ReactivityUpdatingArraysAndObjects from "./d-updating-arrays-and-objects/index.svelte";
 </script>
 
 <!-- <ReactivityAssignments /> -->
 <!-- <ReactivityDeclarations /> -->
-<ReactivityStatements />
+<!-- <ReactivityStatements /> -->
+<ReactivityUpdatingArraysAndObjects />
---
+++ src/2-reactivity/d-updating-arrays-and-objects/index.svelte
@@ -0,0 +1,13 @@
+<script>
+  let numbers = [1, 2, 3, 4];
+
+  function multiplyNumber() {
+    numbers = [...numbers, numbers.length + 1];
+  }
+
+  $: product = numbers.reduce((t, n) => t * n, numbers[0]);
+</script>
+
+<p>{numbers.join(' + ')} = {product}</p>
+
+<button on:click={multiplyNumber}>Multiply a number</button>
