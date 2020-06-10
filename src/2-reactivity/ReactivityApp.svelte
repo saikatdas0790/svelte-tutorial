902d9f370b767b76c8902a8110ba96bb87488db3
Saikat Das <saikatdas0790@gmail.com>
2020-06-10 23:49:59 +0530

    reactivity - statements

--- src/2-reactivity/ReactivityApp.svelte
+++ src/2-reactivity/ReactivityApp.svelte
@@ -1,7 +1,9 @@
 <script>
   import ReactivityAssignments from "./a-assignments/index.svelte";
   import ReactivityDeclarations from "./b-declarations/index.svelte";
+  import ReactivityStatements from "./c-statements/index.svelte";
 </script>
 
 <!-- <ReactivityAssignments /> -->
-<ReactivityDeclarations />
+<!-- <ReactivityDeclarations /> -->
+<ReactivityStatements />
---
+++ src/2-reactivity/c-statements/index.svelte
@@ -0,0 +1,23 @@
+<script>
+  let count = 0;
+
+  const handleClick = () => {
+    count++;
+  };
+
+  $: console.log(`the count is ${count}`);
+
+  $: {
+    console.log(`the count is ${count}`);
+    console.error(`I SAID THE COUNT IS ${count}`);
+  }
+
+  $: if (count >= 10) {
+    alert(`count is dangerously high!`);
+    count = 9;
+  }
+</script>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
