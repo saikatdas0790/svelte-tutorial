f8a2863fa03d363830f5cf61a1cc0083b932d8a4
Saikat Das <saikatdas0790@gmail.com>
2020-06-10 23:42:25 +0530

    reactivity - declarations

--- src/2-reactivity/ReactivityApp.svelte
+++ src/2-reactivity/ReactivityApp.svelte
@@ -1,5 +1,7 @@
 <script>
   import ReactivityAssignments from "./a-assignments/index.svelte";
+  import ReactivityDeclarations from "./b-declarations/index.svelte";
 </script>
 
-<ReactivityAssignments></ReactivityAssignments>
+<!-- <ReactivityAssignments /> -->
+<ReactivityDeclarations />
