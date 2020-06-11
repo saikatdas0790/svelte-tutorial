1bf49e91852bd74d6ebea3b774ce7346c6c3af29
Saikat Das <saikatdas0790@gmail.com>
2020-06-11 10:47:33 +0530

    props - declaring props

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import IntroductionApp from "./1-introduction/IntroductionApp.svelte";
 import ReactivityApp from "./2-reactivity/ReactivityApp.svelte";
+import PropsApp from "./3-props/PropsApp.svelte";
 
 // const app = new IntroductionApp({
 //   target: document.body,
 // });
-const app = new ReactivityApp({
+
+// const app = new ReactivityApp({
+//   target: document.body,
+// });
+
+const app = new PropsApp({
   target: document.body,
 });
 
