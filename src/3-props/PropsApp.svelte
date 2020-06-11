7406554c21ec6e65033001a6674a403fe4640d14
Saikat Das <saikatdas0790@gmail.com>
2020-06-11 14:58:35 +0530

    props - spread - props

--- src/3-props/PropsApp.svelte
+++ src/3-props/PropsApp.svelte
@@ -1,5 +1,9 @@
 <script>
   import PropsDeclaringProps from "./a-declaring-props/index.svelte";
+  import PropsDefaultValues from "./b-default-values/index.svelte";
+  import PropsSpreadProps from "./c-spread-props/index.svelte";
 </script>
 
-<PropsDeclaringProps />
+<!-- <PropsDeclaringProps /> -->
+<!-- <PropsDefaultValues /> -->
+<PropsSpreadProps />
