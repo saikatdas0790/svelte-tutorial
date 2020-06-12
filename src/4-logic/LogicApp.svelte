0fc70f34cb096808a66a39d396cd05c6a75d343d
Saikat Das <saikatdas0790@gmail.com>
2020-06-12 13:01:46 +0530

    logic - each blocks

--- src/4-logic/LogicApp.svelte
+++ src/4-logic/LogicApp.svelte
@@ -2,8 +2,10 @@
   import LogicIfBlocks from "./a-if-blocks/index.svelte";
   import LogicElseBlocks from "./b-else-blocks/index.svelte";
   import LogicElseIfBlocks from "./c-else-if-blocks/index.svelte";
+  import LogicEachBlocks from "./d-each-blocks/index.svelte";
 </script>
 
 <!-- <LogicIfBlocks /> -->
 <!-- <LogicElseBlocks /> -->
-<LogicElseIfBlocks />
+<!-- <LogicElseIfBlocks /> -->
+<LogicEachBlocks />
