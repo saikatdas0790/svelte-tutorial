2306634332edebdcf6aae4805b5a77419272eb4a
Saikat Das <saikatdas0790@gmail.com>
2020-06-12 12:37:19 +0530

    logic - else if blocks

--- src/4-logic/LogicApp.svelte
+++ src/4-logic/LogicApp.svelte
@@ -1,7 +1,9 @@
 <script>
   import LogicIfBlocks from "./a-if-blocks/index.svelte";
   import LogicElseBlocks from "./b-else-blocks/index.svelte";
+  import LogicElseIfBlocks from "./c-else-if-blocks/index.svelte";
 </script>
 
 <!-- <LogicIfBlocks /> -->
-<LogicElseBlocks />
+<!-- <LogicElseBlocks /> -->
+<LogicElseIfBlocks />
