563c8fcc7641066a83798d46d8a4f9cd1873346f
Saikat Das <saikatdas0790@gmail.com>
2020-06-14 09:39:47 +0530

    logic - keyed each blocks

--- src/4-logic/LogicApp.svelte
+++ src/4-logic/LogicApp.svelte
@@ -3,9 +3,11 @@
   import LogicElseBlocks from "./b-else-blocks/index.svelte";
   import LogicElseIfBlocks from "./c-else-if-blocks/index.svelte";
   import LogicEachBlocks from "./d-each-blocks/index.svelte";
+  import LogicKeyedEachBlocks from "./e-keyed-each-blocks/index.svelte";
 </script>
 
 <!-- <LogicIfBlocks /> -->
 <!-- <LogicElseBlocks /> -->
 <!-- <LogicElseIfBlocks /> -->
-<LogicEachBlocks />
+<!-- <LogicEachBlocks /> -->
+<LogicKeyedEachBlocks />
