f899ff05a580baf5a9ee91f08897ec12cf8cb227
Saikat Das <saikatdas0790@gmail.com>
2020-06-17 19:43:41 +0530

    events - event forwarding

--- src/5-events/EventsApp.svelte
+++ src/5-events/EventsApp.svelte
@@ -3,9 +3,11 @@
   import EventsInlineHandlers from "./b-inline-handlers/index.svelte";
   import EventsEventModifiers from "./c-event-modifiers/index.svelte";
   import EventsComponentEvent from "./d-component-event/index.svelte";
+  import EventsEventForwarding from "./e-event-forwarding/index.svelte";
 </script>
 
 <!-- <EventsDOMEvents /> -->
 <!-- <EventsInlineHandlers /> -->
 <!-- <EventsEventModifiers /> -->
-<EventsComponentEvent />
+<!-- <EventsComponentEvent /> -->
+<EventsEventForwarding />
