ed66dd0dc7baa67c6b6f0d0fe92d5b907b4b9843
Saikat Das <saikatdas0790@gmail.com>
2020-06-17 22:28:40 +0530

    bindings = numeric inputs

--- src/6-bindings/BindingsApp.svelte
+++ src/6-bindings/BindingsApp.svelte
@@ -1,5 +1,7 @@
 <script>
   import BindingsTextInputs from "./a-text-inputs/index.svelte";
+  import BindingsNumericInputs from "./b-numeric-inputs/index.svelte";
 </script>
 
-<BindingsTextInputs />
+<!-- <BindingsTextInputs /> -->
+<BindingsNumericInputs />
