70820c4502e300b461680774cbe12cefd8f51767
Saikat Das <saikatdas0790@gmail.com>
2020-06-17 22:34:37 +0530

    bindings - checkbox inputs

--- src/6-bindings/BindingsApp.svelte
+++ src/6-bindings/BindingsApp.svelte
@@ -1,7 +1,9 @@
 <script>
   import BindingsTextInputs from "./a-text-inputs/index.svelte";
   import BindingsNumericInputs from "./b-numeric-inputs/index.svelte";
+  import BindingsCheckboxInputs from "./c-checkbox-inputs/index.svelte";
 </script>
 
 <!-- <BindingsTextInputs /> -->
-<BindingsNumericInputs />
+<!-- <BindingsNumericInputs /> -->
+<BindingsCheckboxInputs />
