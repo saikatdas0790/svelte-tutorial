317d765d6ad5ed2cedce4980d42bb72a42ef09d5
Saikat Das <saikatdas0790@gmail.com>
2020-06-18 18:32:47 +0530

    bindings - select bindings

--- src/6-bindings/BindingsApp.svelte
+++ src/6-bindings/BindingsApp.svelte
@@ -4,10 +4,12 @@
   import BindingsCheckboxInputs from "./c-checkbox-inputs/index.svelte";
   import BindingsGroupInputs from "./d-group-inputs/index.svelte";
   import BindingsTextAreaInputs from "./e-textarea-inputs/index.svelte";
+  import BindingsSelectBindings from "./f-select-bindings/index.svelte";
 </script>
 
 <!-- <BindingsTextInputs /> -->
 <!-- <BindingsNumericInputs /> -->
 <!-- <BindingsCheckboxInputs /> -->
 <!-- <BindingsGroupInputs /> -->
-<BindingsTextAreaInputs />
+<!-- <BindingsTextAreaInputs /> -->
+<BindingsSelectBindings />
