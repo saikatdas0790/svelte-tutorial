4fb94bc7fa3aae388d75bd649ced6f277a284734
Saikat Das <saikatdas0790@gmail.com>
2020-06-23 22:19:26 +0530

    lifecycle - beforeupdate and afterupdate

--- src/7-lifecycle/LifecycleApp.svelte
+++ src/7-lifecycle/LifecycleApp.svelte
@@ -1,7 +1,9 @@
 <script>
   import LifecycleOnMount from "./a-onmount/index.svelte";
   import LifecycleOnDestroy from "./b-ondestroy/index.svelte";
+  import LifecycleBeforeUpdateAndAfterUpdate from "./c-beforeupdate-and-afterupdate/index.svelte";
 </script>
 
 <!-- <LifecycleOnMount></LifecycleOnMount> -->
-<LifecycleOnDestroy></LifecycleOnDestroy>
+<!-- <LifecycleOnDestroy></LifecycleOnDestroy> -->
+<LifecycleBeforeUpdateAndAfterUpdate />
