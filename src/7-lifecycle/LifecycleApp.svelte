0c615a12ab1ec9c9b99581d3119cbb581cd5342d
Saikat Das <saikatdas0790@gmail.com>
2020-06-23 22:54:20 +0530

    lifecycle - tick

--- src/7-lifecycle/LifecycleApp.svelte
+++ src/7-lifecycle/LifecycleApp.svelte
@@ -2,8 +2,10 @@
   import LifecycleOnMount from "./a-onmount/index.svelte";
   import LifecycleOnDestroy from "./b-ondestroy/index.svelte";
   import LifecycleBeforeUpdateAndAfterUpdate from "./c-beforeupdate-and-afterupdate/index.svelte";
+  import LifecycleTick from "./d-tick/index.svelte";
 </script>
 
 <!-- <LifecycleOnMount></LifecycleOnMount> -->
 <!-- <LifecycleOnDestroy></LifecycleOnDestroy> -->
-<LifecycleBeforeUpdateAndAfterUpdate />
+<!-- <LifecycleBeforeUpdateAndAfterUpdate /> -->
+<LifecycleTick />
