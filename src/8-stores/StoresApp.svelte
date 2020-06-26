ea4177932e9b7488367366132f6b242863378241
Saikat Das <saikatdas0790@gmail.com>
2020-06-26 13:06:26 +0530

    stores - derived stores

--- src/8-stores/StoresApp.svelte
+++ src/8-stores/StoresApp.svelte
@@ -2,8 +2,10 @@
   import StoresWritableStores from "./a-writable-stores/index.svelte";
   import StoresAutoSubscriptions from "./b-auto-subscriptions/index.svelte";
   import StoresReadableStores from "./c-readable-stores/index.svelte";
+  import StoresDerivedStores from "./d-derived-stores/index.svelte";
 </script>
 
 <!-- <StoresWritableStores /> -->
 <!-- <StoresAutoSubscriptions /> -->
-<StoresReadableStores />
+<!-- <StoresReadableStores /> -->
+<StoresDerivedStores />
---
+++ src/8-stores/d-derived-stores/stores.js
@@ -0,0 +1,12 @@
+import { readable, derived } from "svelte/store";
+
+export const time = readable(new Date(), (set) => {
+  const interval = setInterval(() => set(new Date()), 1000);
+  return () => clearInterval(interval);
+});
+
+const start = new Date();
+
+export const elapsed = derived(time, ($time) =>
+  Math.round(($time - start) / 1000)
+);
