11c0a433b9e43cabb13c1c92b311bf317783fc5e
Saikat Das <saikatdas0790@gmail.com>
2020-06-26 11:16:49 +0530

    stores - readable stores

--- src/8-stores/StoresApp.svelte
+++ src/8-stores/StoresApp.svelte
@@ -1,7 +1,9 @@
 <script>
   import StoresWritableStores from "./a-writable-stores/index.svelte";
   import StoresAutoSubscriptions from "./b-auto-subscriptions/index.svelte";
+  import StoresReadableStores from "./c-readable-stores/index.svelte";
 </script>
 
 <!-- <StoresWritableStores /> -->
-<StoresAutoSubscriptions />
+<!-- <StoresAutoSubscriptions /> -->
+<StoresReadableStores />
