9aaca3ee916876f5cb4ea18e963211a3ef3eac12
Saikat Das <saikatdas0790@gmail.com>
2020-06-28 13:00:15 +0530

    stores - custom stores

--- src/8-stores/StoresApp.svelte
+++ src/8-stores/StoresApp.svelte
@@ -3,9 +3,11 @@
   import StoresAutoSubscriptions from "./b-auto-subscriptions/index.svelte";
   import StoresReadableStores from "./c-readable-stores/index.svelte";
   import StoresDerivedStores from "./d-derived-stores/index.svelte";
+  import StoresCustomStores from "./e-custom-stores/index.svelte";
 </script>
 
 <!-- <StoresWritableStores /> -->
 <!-- <StoresAutoSubscriptions /> -->
 <!-- <StoresReadableStores /> -->
-<StoresDerivedStores />
+<!-- <StoresDerivedStores /> -->
+<StoresCustomStores />
