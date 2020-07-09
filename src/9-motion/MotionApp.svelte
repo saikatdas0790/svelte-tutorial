81e81b130b02de87a1ab5296a38593ec0d111871
Saikat Das <saikatdas0790@gmail.com>
2020-07-09 18:34:08 +0530

    motion - spring

--- src/9-motion/MotionApp.svelte
+++ src/9-motion/MotionApp.svelte
@@ -1,5 +1,7 @@
 <script>
   import MotionTweened from "./a-tweened/index.svelte";
+  import MotionSpring from "./b-spring/index.svelte";
 </script>
 
-<MotionTweened />
+<!-- <MotionTweened /> -->
+<MotionSpring />
