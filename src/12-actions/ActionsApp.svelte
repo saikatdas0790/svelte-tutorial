906f7c9d8c24bc25f370551fb0de2f7720074992
Saikat Das <saikatdas0790@gmail.com>
2020-07-15 18:31:52 +0530

    actions - adding parameters

--- src/12-actions/ActionsApp.svelte
+++ src/12-actions/ActionsApp.svelte
@@ -1,5 +1,7 @@
 <script>
   import ActionsTheUseDirective from "./a-the-use-directive/index.svelte";
+  import ActionsAddingParameters from "./b-adding-parameters/index.svelte";
 </script>
 
-<ActionsTheUseDirective />
+<!-- <ActionsTheUseDirective /> -->
+<ActionsAddingParameters />
