ab8e0eb4d9cc7d79fed138d52c714e7dc45161e2
Saikat Das <saikatdas0790@gmail.com>
2020-07-15 18:42:03 +0530

    classes - shorthand class directive

--- src/13-classes/ClassesApp.svelte
+++ src/13-classes/ClassesApp.svelte
@@ -1,5 +1,7 @@
 <script>
   import ClassesTheClassDirective from "./a-the-class-directive/index.svelte";
+  import ClassesShorthandClassDirective from "./b-shorthand-class-directive/index.svelte";
 </script>
 
-<ClassesTheClassDirective />
+<!-- <ClassesTheClassDirective /> -->
+<ClassesShorthandClassDirective />
