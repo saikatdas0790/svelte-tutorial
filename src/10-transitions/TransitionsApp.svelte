60e948636559931dd46574e8bd576ee5d7bcddb1
Saikat Das <saikatdas0790@gmail.com>
2020-07-15 16:54:57 +0530

    transitions - in and out

--- src/10-transitions/TransitionsApp.svelte
+++ src/10-transitions/TransitionsApp.svelte
@@ -1,7 +1,9 @@
 <script>
   import TransitionsTheTransitionDirective from "./a-the-transition-directive/index.svelte";
   import TransitionsAddingParameters from "./b-adding-parameters/index.svelte";
+  import TransitionsInAndOut from "./c-in-and-out/index.svelte";
 </script>
 
 <!-- <TransitionsTheTransitionDirective /> -->
-<TransitionsAddingParameters />
+<!-- <TransitionsAddingParameters /> -->
+<TransitionsInAndOut />
