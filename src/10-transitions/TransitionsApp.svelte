d3c6a1dfba6d55402ed7abea924465fbe63d2d41
Saikat Das <saikatdas0790@gmail.com>
2020-07-15 17:05:06 +0530

    transitions - custom css transitions

--- src/10-transitions/TransitionsApp.svelte
+++ src/10-transitions/TransitionsApp.svelte
@@ -2,8 +2,10 @@
   import TransitionsTheTransitionDirective from "./a-the-transition-directive/index.svelte";
   import TransitionsAddingParameters from "./b-adding-parameters/index.svelte";
   import TransitionsInAndOut from "./c-in-and-out/index.svelte";
+  import TransitionsCustomCSSTransitions from "./d-custom-css-transitions/index.svelte";
 </script>
 
 <!-- <TransitionsTheTransitionDirective /> -->
 <!-- <TransitionsAddingParameters /> -->
-<TransitionsInAndOut />
+<!-- <TransitionsInAndOut /> -->
+<TransitionsCustomCSSTransitions />
