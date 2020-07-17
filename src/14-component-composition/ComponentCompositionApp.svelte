e5ca11b7aea582c4cc53814c7d6b760a76187068
Saikat Das <saikatdas0790@gmail.com>
2020-07-17 19:37:14 +0530

    component composition - named slots

--- src/14-component-composition/ComponentCompositionApp.svelte
+++ src/14-component-composition/ComponentCompositionApp.svelte
@@ -1,7 +1,9 @@
 <script>
   import ComponentCompositionSlots from "./a-slots/index.svelte";
   import ComponentCompositionSlotFallbacks from "./b-slot-fallbacks/index.svelte";
+  import ComponentCompositionNamedSlots from "./c-named-slots/index.svelte";
 </script>
 
 <!-- <ComponentCompositionSlots /> -->
-<ComponentCompositionSlotFallbacks />
+<!-- <ComponentCompositionSlotFallbacks /> -->
+<ComponentCompositionNamedSlots />
