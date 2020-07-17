6979aca6491fba5d10f3317629bbe37a2924a8ad
Saikat Das <saikatdas0790@gmail.com>
2020-07-17 23:44:40 +0530

    special elements - svelte window

--- src/16-special-elements/SpecialElementsApp.svelte
+++ src/16-special-elements/SpecialElementsApp.svelte
@@ -1,7 +1,9 @@
 <script>
   import SpecialElementsSvelteSelf from "./a-svelte-self/index.svelte";
   import SpecialElementsSvelteComponent from "./b-svelte-component/index.svelte";
+  import SpecialElementsSvelteWindow from "./c-svelte-window/index.svelte";
 </script>
 
 <!-- <SpecialElementsSvelteSelf /> -->
-<SpecialElementsSvelteComponent />
+<!-- <SpecialElementsSvelteComponent /> -->
+<SpecialElementsSvelteWindow />
