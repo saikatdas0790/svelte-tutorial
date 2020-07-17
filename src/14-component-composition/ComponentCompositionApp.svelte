da70b1caf1e3bf2cbeb19132a84fd4b550f28e96
Saikat Das <saikatdas0790@gmail.com>
2020-07-17 19:20:05 +0530

    component composition - slot fallbacks

--- src/14-component-composition/ComponentCompositionApp.svelte
+++ src/14-component-composition/ComponentCompositionApp.svelte
@@ -1,5 +1,7 @@
 <script>
   import ComponentCompositionSlots from "./a-slots/index.svelte";
+  import ComponentCompositionSlotFallbacks from "./b-slot-fallbacks/index.svelte";
 </script>
 
-<ComponentCompositionSlots />
+<!-- <ComponentCompositionSlots /> -->
+<ComponentCompositionSlotFallbacks />
