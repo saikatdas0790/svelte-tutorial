708e4fbefe6dbc775073321f687ef336dba93196
Saikat Das <saikatdas0790@gmail.com>
2020-07-18 18:11:41 +0530

    special elements - svelte head

--- src/16-special-elements/SpecialElementsApp.svelte
+++ src/16-special-elements/SpecialElementsApp.svelte
@@ -4,10 +4,12 @@
   import SpecialElementsSvelteWindow from "./c-svelte-window/index.svelte";
   import SpecialElementsSvelteWindowBindings from "./d-svelte-window-bindings/index.svelte";
   import SpecialElementsSvelteBody from "./e-svelte-body/index.svelte";
+  import SpecialElementsSvelteHead from "./f-svelte-head/index.svelte";
 </script>
 
 <!-- <SpecialElementsSvelteSelf /> -->
 <!-- <SpecialElementsSvelteComponent /> -->
 <!-- <SpecialElementsSvelteWindow /> -->
 <!-- <SpecialElementsSvelteWindowBindings /> -->
-<SpecialElementsSvelteBody />
+<!-- <SpecialElementsSvelteBody /> -->
+<SpecialElementsSvelteHead />
---
+++ src/16-special-elements/f-svelte-head/index.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+  <link rel="stylesheet" href="https://svelte.dev/tutorial/dark-theme.css" />
+</svelte:head>
+
+<h1>Hello world!</h1>
