236efcea84dae1d883ad56e07387a7c6c1b0d87f
Saikat Das <saikatdas0790@gmail.com>
2021-01-20 15:33:40 +0530

    style: complete css setup

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,18 @@
   export let name: string;
 </script>
 
+<style>
+  h1 {
+    background-color: teal;
+  }
+</style>
+
 <main>
-  <h1>Hello {name}!</h1>
+  <h1 class="title">Hello {name}!</h1>
   <p>
     Visit the
     <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
     to learn how to build Svelte apps.
   </p>
+  <button class="button is-primary">Click me</button>
 </main>
